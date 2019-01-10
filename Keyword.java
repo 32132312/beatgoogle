862766291cf68657101aee39073145b53e732d1a
timmychien <46017390+timmychien@users.noreply.github.com>
2019-01-10 13:21:26 +0800

    Add files via upload

---
+++ Keyword.java
@@ -0,0 +1,14 @@
+
+public class Keyword {
+	public String name;
+	public int count;
+	public Keyword(String name,int count) {
+		this.name=name;
+		this.count=count;
+	}
+	public String toString() {
+		return"{name="+name+"count="+count+"}";
+	}
+	}
+
+
